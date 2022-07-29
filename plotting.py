51aa1fec8918153461928d0bef5a9b4923f23cff
Mason Pott <mason.pott@slalom.com>
2022-07-29 12:45:34 -0700

    added hover

--- plotting.py
+++ plotting.py
@@ -1,12 +1,21 @@
 from bokeh.plotting import figure, show, output_file
+from bokeh.models import HoverTool, ColumnDataSource
 import pandas
-from yaml import parse
 
 df = pandas.read_csv("Sample_of_the_produced_time_values.csv", parse_dates=["Start", "End"])
 
-p = figure(x_axis_type = 'datetime', height = 100, width = 500, title = "Motion Graph", sizing_mode = "scale_both")
+df["Start_string"] = df["Start"].dt.strftime("%Y-%M-%D %H:%M:%S")
+df["End_string"] = df["End"].dt.strftime("%Y-%M-%D %H:%M:%S")
 
-q = p.quad(left = df["Start"], right = df["End"], bottom = 0, top = 1, color = "green")
+cds = ColumnDataSource(df)
+
+p = figure(x_axis_type = 'datetime', height = 100, width = 500, title = "Motion Graph", sizing_mode = "scale_both")
+p.yaxis.minor_tick_line_color = None
+
+hover = HoverTool(tooltips = [("Start", "@Start_string"), ("End", "@End_string")])
+p.add_tools(hover)
+
+q = p.quad(left = "Start", right = "End", bottom = 0, top = 1, color = "green", source = cds)
 
 output_file('Graph.html')
 
